a81539839e8c69d82f8be3b3a5ea175fddd9331f
Abdullah <abdullahv2730@gmail.com>
2022-08-07 19:43:25 +0300

    Voting For President

---
+++ Challenges/chall-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? str = "";
+
+            while (str != "exit")
+            {
+                try
+                {
+                    Console.WriteLine("Enter age");
+                    str = Console.ReadLine();
+                    if (str == "exit") break;
+                    int age = Int32.Parse(str);
+                    Console.WriteLine(CalculatePresidents(age));
+
+                }
+                catch (FormatException) { Console.WriteLine("only type in integer value");}
+            }
+        }
+
+        private static int CalculatePresidents(int age)
+        {
+            int result = 0, minVotingAge = 18;
+
+            if (age < minVotingAge)
+            {
+                return 0;
+            }
+            else
+            {
+                result = (age - minVotingAge) / 4;
+            }
+            return result;
+        }
+    }
+}
